398c3f17c882941042d73bda9986a791072a04ae
Sean Rose <sean1rose@gmail.com>
2017-03-11 00:35:27 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+// can set variable that's equal to teh result of the exported module/function in greet.js
+var greet = require('./greet');
+greet();
---
+++ greet.js
@@ -0,0 +1,6 @@
+var greet = () => {
+  console.log('Hello!');
+};
+
+// makes greet available for use outside of this file
+module.exports = greet;
