332342f1d0e705ed0fcd338bff0045183db6ca76
Sean Rose <sean1rose@gmail.com>
2017-03-16 23:53:53 -0700

    event emitter pt 2

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 // event emmitter
-var Emitter = require('./emitter');
+var Emitter = require('events');
+
+// try to avoid using magic strings by assigning the string to a variable
+var eventConfig = require('./config').events;
 
 var emtr = new Emitter();
-console.log('emtr - ', emtr);
 
 // Will run whenever 'greet' event is emitted:
 
-// THese listeners are just functions sitting in an array
+// These listeners are just functions sitting in an array
 
 // 'greet' is the prop name on the obj, that will hold all of the events
-emtr.on('greet', function() {
+emtr.on(eventConfig.GREET, function() {
   console.log('Somewhere, someone said hello.');
 });
 
-emtr.on('greet', function() {
+emtr.on(eventConfig.GREET, function() {
   console.log('A greeting occurred!');
 });
 
 console.log('Hello!');
 
 // manually emit the event (loop thru teh array and invoke all of the functions)
-emtr.emit('greet');
+emtr.emit(eventConfig.GREET);
