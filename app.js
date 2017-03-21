97148ae9d63787988d560cde33c258101abaaa61
Sean Rose <sean1rose@gmail.com>
2017-03-20 22:55:56 -0700

    read and write streams

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
+// streams -> sequence of pieces/chunks of data
 var fs = require('fs');
-// file system. wraps up c++ code to deal w/ lower level files
 
-// SYNCRONOUS
-// can get data from the text file (can read from file on the file sys (binary data))
-  // 1st arg: full path on my hard drive to the greet.txt file...
-  // 2nd arg: specified encoding (utf8 is default), used to determine what characters 0s and 1s map to
-var greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
-  // sync means async -> syncrhonous method so that will wait til buffer is filled an has string back b4 moving on to next line of code...
+// give me back the readstream obj thats returned
+// stream will begin to fill buffer (pieces at a time) (if don't pass encoding option paramater)
+  // this will return a buffer...
+// var readable = fs.createReadStream(__dirname +
+//  '/greet.txt');
+
+// if you pass createReadStream function an object as 3rd parameter, w/ encoding, it will encode the buffer into a string
+// var readable = fs.createReadStream(__dirname +
+//  '/greet.txt', {encoding: 'utf8'});
 
 
-// ASYNCRONOUS
-  // error-first callback: callbacks take an error obj as their 1st paramater  see how the err obj is the 1st paramater in the callback func...
-var greet2 = fs.readFile(__dirname + '/greet.txt', 'utf8', function(err, data) {
-  // async -> reads contents of file then takes a callback,
-  // once libuv is done reading the file, then invokes this callback func
-  console.log('data -> ', data);
-    // ^that returns a buffer <Buffer 48 65 6c 6c 20 77 6f 72 6c 64 21>
-    // to convert ^ to a string, pass in 'utf8' as 2nd parameter of readFile
+var readable = fs.createReadStream(__dirname +
+ '/greet.txt', {encoding: 'utf8', highWaterMark: 16 * 1024});
+ // ^ highWaterMark will tell you what size u want each chunk to be...
+ // so don't need to read whole file at once, can send in pieces/chunks
+
+// create a writeable stream
+var writable = fs.createWriteStream(__dirname + 
+'/greetcopy.txt');
+
+// can listen for event emitted by read stream (data event)
+readable.on('data', function(chunk) {
+  console.log(chunk);
+  // on every chunk -> write
+  writable.write(chunk);
+
+
+  // ^^^ so read from readable and write to writable...
 });
