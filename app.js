6437c8a1ccde2c1bf4ae6910b2ff334932d74edc
Sean Rose <sean1rose@gmail.com>
2017-03-21 22:31:10 -0700

    using express routing pattern matching

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.get('/', function(req, res) {
   res.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
 });
 
+// ':' means 'id' could be anything -> accessed via req.params.id
+app.get('/person/:id', function(req, res) {
+  // express automatically adds content-type to header
+  res.send('<html><head></head><body><h1>Person: ' + req.params.id  + '</h1></body></html>');
+});
+
+
 app.get('/api', function(req, res) {
   res.json({ firstname: 'John', lastname: 'Doe' });
 });
