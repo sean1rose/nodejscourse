21b9c1c3b6b0bc3b6b715d60505eb63428d46d4d
Sean Rose <sean1rose@gmail.com>
2017-03-22 23:32:08 -0700

    start middleware vid

--- app.js
+++ app.js
@@ -12,10 +12,18 @@ app.get('/', function(req, res) {
   res.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
 });
 
-// ':' means 'id' could be anything -> accessed via req.params.id
+// still need to deliver static files when they're requested
+  // to make sure a public static file can be delivered -> use MIDDLEWARE
+  // every time see '/assets', go find this full file path and look for that same file and stream the response back
+  // any static file would be available by hitting '/assets'
+  app.use('/assets', express.static(__dirname + '/public'));
+
+
+// pull data from the url using pattern matching to pull values out of the routes
+  // ':' means 'id' could be anything -> accessed via req.params.id
 app.get('/person/:id', function(req, res) {
-  // express automatically adds content-type to header
-  res.send('<html><head></head><body><h1>Person: ' + req.params.id  + '</h1></body></html>');
+  // browser will see this link tag, will generate http request for /assets/style.css, handled by node, when it sees '/assets', will know to look in public folder, and will respond by streaming the static file
+  res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Person: ' + req.params.id  + '</h1></body></html>');
 });
 
 
